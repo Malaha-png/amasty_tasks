9f07dc59051af23f177ae3e7281fea2a7228e756
Ilya Malashko <ilya.malashko1999@yandex.by>
2020-12-02 01:39:23 +0300

    init

--- task4.php
+++ task4.php
@@ -9,10 +9,16 @@
 
 <body>
    <?php
+   require_once './simplehtmldom/simple_html_dom.php';
 
-   if (isset($_POST['com'])) {
-      require_once './simplehtmldom/simple_html_dom.php';
+   $html = file_get_html('https://terrikon.com/football/italy/championship/');
 
+   foreach ($html->find('table.grouptable  a') as $element) {
+      echo $element->plaintext . '<br>';
+   }
+   // echo empty($_POST['com']) . ' com ';
+   $value = trim($_POST['com']);
+   if (isset($value) && !empty($value)) {
       $html = file_get_html('https://terrikon.com/football/italy/championship/');
 
       $search = trim(mb_strtolower($_POST['com']));
@@ -23,23 +29,23 @@
          if (!($pos === false)) {
             $href =  $element->href;
             $html = file_get_html('https://terrikon.com' . $href . '/trophy');
-            foreach ($html->find('.maincol .main div') as $element) {
-               echo $element;
-               // $val = $element->plaintext;
-               // $val = trim(mb_strtolower($val));
-               // $pos = strpos($val, $search);
-               // if (!($pos === false)) {
-               //    $href =  $element->href;
-               //    $html = file_get_html('https://terrikon.com' . $href);
-               //    echo $element->href . '<br>';
-               // }
+            $html = $html->find('.maincol .main div');
+            if (!empty($html)) {
+               echo '<p> Ваша команда <strong>' . $_POST['com'] . '</strong></p>';
+               foreach ($html as $element) {
+                  echo $element;
+               }
+            } else {
+               echo '<p>Архив команды <strong>' . $_POST['com'] . '</strong> не найден </p>';
             }
          }
       }
    }
    ?>
 
+   <br>
    <form action="" method="post">
+      <p>Введите конманду</p>
       <input type="text" name="com">
       <button type="submit">Получить</button>
    </form>
